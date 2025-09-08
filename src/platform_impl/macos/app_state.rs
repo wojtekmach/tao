ac4e920611d22aaffb1b0ab21206295ffd47f2bf
Wojtek Mach <wojtek@wojtekmach.pl>
2025-09-08 13:21:05 +0200

    up

--- src/platform_impl/macos/app_state.rs
+++ src/platform_impl/macos/app_state.rs
@@ -462,11 +462,11 @@ fn apply_launch_activation_policy(app_delegate: &Object) {
     // to regular (default), accessory (LSUIElement=1), or prohibited (LSBackgroundOnly=1)
     // so let's not overwrite it.
     if NSBundle::mainBundle().bundlePath().hasSuffix(&NSString::from_str(".app")) {
-        dbg!("skip");
+        // dbg!("skip");
         return;
     }
 
-    dbg!("continue");
+    // dbg!("continue");
 
     // We need to delay setting the activation policy and activating the app
     // until `applicationDidFinishLaunching` has been called. Otherwise the
